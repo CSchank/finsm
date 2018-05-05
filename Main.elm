3807d9297d5c47722bc95accfd03c53a8931ab7d
CSchank <chris@schankula.ca>
2018-05-05 16:56:46 -0400

    many additions to arrows

--- Main.elm
+++ Main.elm
@@ -29,11 +29,21 @@ type alias InputTape =
     Array Character
 
 
+type alias StatePositions =
+    Dict State ( Float, Float )
+
+
+type alias StateTransitions =
+    Dict ( State, State ) ( Float, Float )
+
+
 type alias Model =
     { machine : Machine
     , states : Set State
     , input : Array Character
     , inputAt : Int
+    , statePositions : StatePositions
+    , stateTransitions : StateTransitions
     }
 
 
@@ -94,7 +104,15 @@ test =
 
 main =
     cmdApp Tick
-        { init = ( { machine = test, states = test.start, input = Array.fromList [ "0", "0", "0", "1", "1", "0", "1", "0", "1", "0" ], inputAt = 0 }, Cmd.none )
+        { init =
+            ( { machine = test
+              , states = test.start
+              , input = Array.fromList [ "0", "0", "0", "1", "1", "0", "1", "0", "1", "0" ]
+              , inputAt = 0
+              , statePositions = Dict.fromList [ ( "q0", ( -30, 30 ) ), ( "q1", ( 30, 30 ) ), ( "q2", ( 30, -30 ) ), ( "q3", ( -30, -30 ) ) ]
+              }
+            , Cmd.none
+            )
         , update = update
         , view = view
         , subscriptions = \_ -> Sub.none
@@ -171,6 +189,110 @@ renderTape input inputAt =
                 )
 
 
+
+--renderStates : Set State -> StatePositions
+
+
+renderStates states currents finals pos =
+    let
+        stateList =
+            Set.toList states
+
+        getPos state =
+            case (Dict.get state pos) of
+                Just ( x, y ) ->
+                    ( x, y )
+
+                Nothing ->
+                    ( 0, 0 )
+
+        thickness state =
+            (if Set.member state currents then
+                2
+             else
+                1
+            )
+    in
+        group <|
+            List.map
+                (\state ->
+                    group
+                        [ circle 20
+                            |> outlined (solid (thickness state)) black
+                        , if (Set.member state finals) then
+                            circle 23 |> outlined (solid (thickness state)) black
+                          else
+                            group []
+                        , text state |> centered |> filled black |> move ( 0, -3 )
+                        ]
+                        |> move (getPos state)
+                )
+                stateList
+
+
+arrow ( x0, y0 ) ( x1, y1 ) ( x2, y2 ) =
+    let
+        ( dx, dy ) =
+            ( x2 - x1, y2 - y1 )
+    in
+        group
+            [ curve ( x0, y0 )
+                [ Pull ( x1, y1 )
+                    ( x2 - 2 * cos (atan2 dy dx)
+                    , y2 - 2 * sin (atan2 dy dx)
+                    )
+                ]
+                |> outlined (solid 1) black
+            , triangle 4
+                |> filled black
+                |> rotate (atan2 dy dx)
+                |> move ( x2 - 4 * cos (atan2 dy dx), y2 - 4 * sin (atan2 dy dx) )
+            ]
+
+
+renderArrow ( x0, y0 ) ( x1, y1 ) ( x2, y2 ) r0 r1 char =
+    let
+        ( mx, my ) =
+            ( (x2 + x0) / 2 + x1, (y2 + y0) / 2 + y1 )
+
+        ( dx0, dy0 ) =
+            ( mx - x0, my - y0 )
+
+        ( dx1, dy1 ) =
+            ( mx - x2, my - y2 )
+
+        ( xx0, yy0 ) =
+            ( x0 + r0 * cos (atan2 dy0 dx0), y0 + r0 * sin (atan2 dy0 dx0) )
+
+        ( xx1, yy1 ) =
+            ( x2 + r1 * cos (atan2 dy1 dx1), y2 + r1 * sin (atan2 dy1 dx1) )
+    in
+        group
+            [ arrow ( xx0, yy0 ) ( mx, my ) ( xx1, yy1 )
+            , text char |> centered |> filled black |> move ( mx, my + 5 )
+            ]
+
+
+renderArrows states delta pos =
+    let
+        stateList =
+            Set.toList states
+
+        getPos state =
+            case (Dict.get state pos) of
+                Just ( x, y ) ->
+                    ( x, y )
+
+                Nothing ->
+                    ( 0, 0 )
+    in
+        []
+
+
+
+--List.map (\state -> )
+
+
 view model =
     let
         accepted =
@@ -178,11 +300,8 @@ view model =
     in
         collage 500
             500
-            [ text (toString model.states) |> centered |> filled black
-
-            --, text (toString model.input) |> centered |> filled black |> move ( 0, 30 )
-            , renderTape model.input model.inputAt |> move ( 0, 30 )
-            , text ("inputAt: " ++ toString model.inputAt) |> centered |> filled black |> move ( 0, 60 )
+            [ renderStates model.machine.q model.states model.machine.final model.statePositions |> move ( 0, 100 )
+            , renderTape model.input model.inputAt |> move ( 0, 0 )
             , text ("Accepted: " ++ toString accepted)
                 |> centered
                 |> filled
@@ -191,6 +310,7 @@ view model =
                      else
                         red
                     )
-                |> move ( 0, -30 )
+                |> move ( 0, -60 )
             , group [ roundedRect 30 30 10 |> filled lightGreen, triangle 10 |> filled white ] |> move ( 0, -100 ) |> notifyTap Step
+            , renderArrow ( -30, 30 ) ( 30, 60 ) ( 30, 30 ) 23 20 "a" |> move ( 0, 100 )
             ]
