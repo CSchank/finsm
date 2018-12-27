fe8fe090f696f5c59040e36f847d45d1271bf3ec
CSchank <chris@schankula.ca>
2018-12-27 17:48:45 -0500

    add dummy elm-test support
    Need to create test cases later.

---
+++ tests/Example.elm
@@ -0,0 +1,13 @@
+module Example exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Test exposing (..)
+
+suite : Test
+suite =
+    describe "An easy test"
+        [
+            test "Example test" <|
+                \_ -> Expect.equal 3 3
+        ]
