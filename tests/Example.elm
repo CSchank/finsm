385b29bf3dbb03b30d8699530fb39998db710be4
CSchank <chris@schankula.ca>
2019-11-22 23:46:18 -0500

    run elm-format
    Elm on travis seems to work now?!

--- tests/Example.elm
+++ tests/Example.elm
@@ -2,16 +2,15 @@ module Example exposing (suite)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
-import Test exposing (..)
-import Machine exposing (test)
-
-import Json.Encode as E
 import Json.Decode as D
+import Json.Encode as E
+import Machine exposing (test)
+import Test exposing (..)
 
 
 suite : Test
 suite =
     describe "Machine encoder-decoder"
         [ Test.test "Self-cancellation of encoding and decoding for V1" <|
-                     \_ -> Expect.equal (Ok Machine.test) (D.decodeString Machine.machineDecoderV1 <| E.encode 0 (Machine.machineEncoderV1 Machine.test))
+            \_ -> Expect.equal (Ok Machine.test) (D.decodeString Machine.machineDecoderV1 <| E.encode 0 (Machine.machineEncoderV1 Machine.test))
         ]
